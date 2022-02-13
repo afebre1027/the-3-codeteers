bf6b230373105737443cc42b2803d461ed5b3fdb
joe <hudakjoe@msn.com>
2022-02-13 18:50:25 -0500

    working on games

--- controllers/game-page.js
+++ controllers/game-page.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
 const sequelize = require('../config/connection');
-const{Post, User, Comment, Time} =require('../models');
-const withAuth = require('../utils/auth')
+const { Post, User, Comment, Time } = require('../models');
+const withAuth = require('../utils/auth');
 
 // router.get('/', (req,res)=>{
 //     if(req.session.loggedIn){
@@ -17,66 +17,67 @@ const withAuth = require('../utils/auth')
 //     res.render('login');
 // })
 
-router.get('/', withAuth, (req,res)=>{
-    Promise.all([
-        User.findOne({
-            where:{
-                id: req.session.user_id
-            },
-            attributes:['username', 'email', 'info']
-            // include:[
-            //     {
-            //         model: Comment,
-            //         attributes:['id',
-            //                     'comment_text',
-            //                     'created_at']
-            //     },
-            //     {
-            //         model: Time,
-            //         attributes:['time'],
-            //         limit:5,
-            //         order:['created_at', 'DESC']
-            //     }
-            // ]
-        }),
+router.get('/', withAuth, (req, res) => {
+  Promise.all([
+    User.findOne({
+      where: {
+        id: req.session.user_id,
+      },
+      attributes: ['username', 'email'],
+      // include:[
+      //     {
+      //         model: Comment,
+      //         attributes:['id',
+      //                     'comment_text',
+      //                     'created_at']
+      //     },
+      //     {
+      //         model: Time,
+      //         attributes:['time'],
+      //         limit:5,
+      //         order:['created_at', 'DESC']
+      //     }
+      // ]
+    }),
 
-        Comment.findAll({
-            attributes: ['comment_text', 'created_at'],
-            include:[
-                {
-                    model: User,
-                    attributes: ['username']
-                }
-            ],
-            order:[['created_at', 'DESC']]
-        }),
+    Comment.findAll({
+      attributes: ['comment_text', 'created_at'],
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+      order: [['created_at', 'DESC']],
+    }),
 
-        Time.findAll({
-            attributes:['time'],
-            limit: 10,
-            order:['time'],
-            include:{
-                model:User,
-                attributes:['username']
-            }
-        })
-    ])
-    .then(dbUserData =>{
-        const user=dbUserData[0].get({plain:true});
-        const comments =dbUserData[1].map(comment=>comment.get({plain:true}));
-        const times = dbUserData[2].map(time=>time.get({plain: true}));
+    Time.findAll({
+      attributes: ['time'],
+      limit: 10,
+      order: ['time'],
+      include: {
+        model: User,
+        attributes: ['username'],
+      },
+    }),
+  ]).then((dbUserData) => {
+    const user = dbUserData[0].get({ plain: true });
+    const comments = dbUserData[1].map((comment) =>
+      comment.get({ plain: true })
+    );
+    const times = dbUserData[2].map((time) => time.get({ plain: true }));
 
-        res.render('game-page', {
-        user,
-        comments,
-        times,
-        loggedIn: req.session.loggedIn});
+    res.render('game-page', {
+      user,
+      comments,
+      times,
+      loggedIn: req.session.loggedIn,
     });
-    
+  });
 });
 
 // router.get('/', withAuth, (req,res)=>{
-    
+
 //     .then(dbCommentData =>{
 //         const comments = dbCommentData.map(comment=>comment.get({plain:true}));
 //         res.render('game-page', {
@@ -86,6 +87,4 @@ router.get('/', withAuth, (req,res)=>{
 //     });
 // });
 
-
-
-module.exports=router;
+module.exports = router;
--- models/user.js
+++ models/user.js
@@ -1,59 +1,59 @@
-const {Model, DataTypes} = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
-class User extends Model{
-    checkPassword(loginPw){
-        return bcrypt.compareSync(loginPw, this.password);
-    }
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
 }
 
 User.init(
-    {
-        id:{
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        username:{
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        email:{
-            type: DataTypes.STRING,
-            allowNull: false,
-            //no duplicates
-            unique: true,
-            validate:{
-                isEmail: true
-            }
-        },
-        password:{
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate:{
-                len: [6]
-            }
-        },
-        info:{
-            type: DataTypes.STRING(100),           
-        }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-      hooks:{
-          async beforeCreate(newUserData){
-              newUserData.password = await bcrypt.hash(newUserData.password, 10);
-              return newUserData;
-          }
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      //no duplicates
+      unique: true,
+      validate: {
+        isEmail: true,
       },
-      
-      sequelize,
-      timestamps: false,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'user'  
     },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [6],
+      },
+    },
+    // info: {
+    //   type: DataTypes.STRING(100),
+    // },
+  },
+  {
+    hooks: {
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
+
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
 );
 
 module.exports = User;
