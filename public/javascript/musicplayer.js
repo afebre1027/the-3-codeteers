5161238590f1146fa5320d1b18d919216ce8bb54
joe <hudakjoe@msn.com>
2022-02-16 16:47:00 -0500

    player works and pauses

--- public/javascript/musicplayer.js
+++ public/javascript/musicplayer.js
@@ -1,5 +1,20 @@
 const player = document.getElementById('player');
+let state = {
+  playing: false,
+};
 
-player.addEventListener('click', function ()=> {
-    
-})
+player.addEventListener('click', () => {
+  if (state.playing) {
+    player.innerHTML = '<i class= "fa fa-play"></i> Play Chill Radio';
+    sound.pause();
+    state.playing = false;
+    return;
+  }
+  console.log(state.playing);
+  if (!state.playing) {
+    player.innerHTML = '<i class= "fa fa-pause"></i> Pause Chill Radio';
+    sound.play();
+    state.playing = true;
+    return;
+  }
+});
