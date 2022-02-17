d47d87ff89dd8ebd6bfa7246fd2127fecc869e9a
joe <hudakjoe@msn.com>
2022-02-17 17:51:46 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const sequelize = require('./config/connection');
 const path = require('path');
 const app = express();
+const mysql = require('mysql2');
 const session = require('express-session');
 const routes = require('./controllers/');
 const exphbs = require('express-handlebars');
