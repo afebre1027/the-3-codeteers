e3f7f28ade47d01e71b4c5ac189595a411327b85
joe <hudakjoe@msn.com>
2022-02-20 14:58:54 -0500

    Update game-page.js

--- controllers/game-page.js
+++ controllers/game-page.js
@@ -1,141 +1,143 @@
 const router = require('express').Router();
 
 const sequelize = require('../config/connection');
-const{Post, User, Comment, Score} =require('../models');
-const withAuth = require('../utils/auth')
+const { Post, User, Comment, Score } = require('../models');
+const withAuth = require('../utils/auth');
 
+router.get('/', withAuth, (req, res) => {
+  Promise.all([
+    User.findOne({
+      where: {
+        id: req.session.user_id,
+      },
+      attributes: ['username', 'email'],
+      // include:[
+      //     {
+      //         model: Comment,
+      //         attributes:['id',
+      //                     'comment_text',
+      //                     'created_at']
+      //     },
+      //     {
+      //         model: Score,
+      //         attributes:['Score'],
+      //         limit:5,
+      //         order:['created_at', 'DESC']
+      //     }
+      // ]
+    }),
 
-router.get('/', withAuth, (req,res)=>{
-    Promise.all([
-        User.findOne({
-            where:{
-                id: req.session.user_id
-            },
-            attributes:['username', 'email', 'info']
-            // include:[
-            //     {
-            //         model: Comment,
-            //         attributes:['id',
-            //                     'comment_text',
-            //                     'created_at']
-            //     },
-            //     {
-            //         model: Score,
-            //         attributes:['Score'],
-            //         limit:5,
-            //         order:['created_at', 'DESC']
-            //     }
-            // ]
-        }),
+    Comment.findAll({
+      where: {
+        type: 'general',
+      },
+      attributes: ['comment_text', 'created_at'],
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+      order: [['created_at', 'DESC']],
+    }),
 
-        Comment.findAll({
-            where:{
-                type: 'general'
-            },
-            attributes: ['comment_text', 'created_at'],
-            include:[
-                {
-                    model: User,
-                    attributes: ['username']
-                }
-            ],
-            order:[['created_at', 'DESC']]
-        }),
+    // Score.findAll({
+    //     attributes:['Score'],
+    //     limit: 10,
+    //     order:['Score'],
+    //     include:{
+    //         model:User,
+    //         attributes:['username']
+    //     }
+    // })
+  ]).then((dbUserData) => {
+    const user = dbUserData[0].get({ plain: true });
+    const comments = dbUserData[1].map((comment) =>
+      comment.get({ plain: true })
+    );
+    // const Scores = dbUserData[2].map(Score=>Score.get({plain: true}));
 
-        // Score.findAll({
-        //     attributes:['Score'],
-        //     limit: 10,
-        //     order:['Score'],
-        //     include:{
-        //         model:User,
-        //         attributes:['username']
-        //     }
-        // })
-    ])
-    .then(dbUserData =>{
-        const user=dbUserData[0].get({plain:true});
-        const comments =dbUserData[1].map(comment=>comment.get({plain:true}));
-        // const Scores = dbUserData[2].map(Score=>Score.get({plain: true}));
-
-        res.render('game-page', {
-        user,
-        comments,
-        // Scores,
-        loggedIn: req.session.loggedIn});
+    res.render('game-page', {
+      user,
+      comments,
+      // Scores,
+      loggedIn: req.session.loggedIn,
     });
-    console.log('Cookies: ', req.cookies);
+  });
+  console.log('Cookies: ', req.cookies);
 });
 
-router.get('/floppy', withAuth, (req,res)=>{
-    Promise.all([
-        Comment.findAll({
-            where:{
-                type: 'floppy'
-            },
-            attributes: ['comment_text', 'created_at'],
-            include:[
-                {
-                    model: User,
-                    attributes: ['username']
-                }
-            ],
-            order:[['created_at', 'DESC']]
-        }),
+router.get('/floppy', withAuth, (req, res) => {
+  Promise.all([
+    Comment.findAll({
+      where: {
+        type: 'floppy',
+      },
+      attributes: ['comment_text', 'created_at'],
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+      order: [['created_at', 'DESC']],
+    }),
 
-        User.findOne({
-            where: {
-                id: req.session.user_id
-            },
-            attributes:{exclude:['password']}
-        }),
-    ])
-    .then(dbGameData =>{
-        const comments = dbGameData[0].map(comment=>comment.get({plain: true}));
-        const user = dbGameData[1].get({plain: true});
+    User.findOne({
+      where: {
+        id: req.session.user_id,
+      },
+      attributes: { exclude: ['password'] },
+    }),
+  ]).then((dbGameData) => {
+    const comments = dbGameData[0].map((comment) =>
+      comment.get({ plain: true })
+    );
+    const user = dbGameData[1].get({ plain: true });
 
-        res.render('flappy-page', {
-            comments,
-            user,
-            loggedIn: req.session.loggedIn
-        });
+    res.render('flappy-page', {
+      comments,
+      user,
+      loggedIn: req.session.loggedIn,
     });
-    console.log('Cookies: ', req.cookies);
+  });
+  console.log('Cookies: ', req.cookies);
 });
 
-router.get('/snake', withAuth, (req,res)=>{
-    Promise.all([
-        Comment.findAll({
-            where:{
-                type: 'snake'
-            },
-            attributes: ['comment_text', 'created_at'],
-            include:[
-                {
-                    model: User,
-                    attributes: ['username']
-                }
-            ],
-            order:[['created_at', 'DESC']]
-        }),
+router.get('/snake', withAuth, (req, res) => {
+  Promise.all([
+    Comment.findAll({
+      where: {
+        type: 'snake',
+      },
+      attributes: ['comment_text', 'created_at'],
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+      order: [['created_at', 'DESC']],
+    }),
 
-        User.findOne({
-            where: {
-                id: req.session.user_id
-            },
-            attributes:{exclude:['password']}
-        }),
-    ])
-    .then(dbGameData =>{
-        const comments = dbGameData[0].map(comment=>comment.get({plain: true}));
-        const user = dbGameData[1].get({plain:true});
+    User.findOne({
+      where: {
+        id: req.session.user_id,
+      },
+      attributes: { exclude: ['password'] },
+    }),
+  ]).then((dbGameData) => {
+    const comments = dbGameData[0].map((comment) =>
+      comment.get({ plain: true })
+    );
+    const user = dbGameData[1].get({ plain: true });
 
-        res.render('snake-page', {
-            comments,
-            user,
-            loggedIn: req.session.loggedIn
-        });
+    res.render('snake-page', {
+      comments,
+      user,
+      loggedIn: req.session.loggedIn,
     });
+  });
 });
 
-
-module.exports=router;
+module.exports = router;
